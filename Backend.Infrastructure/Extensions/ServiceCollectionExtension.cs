126658bb6699352facc512e85f5cbb713dc0961a
MarcinWozniak345 <Marinox1234@wp.pl>
2025-10-10 15:23:16 +0200

    Extension method to program.cs - adding my DB context to app

---
+++ Backend.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FootballersDbContext>(options => 
+                options.UseMySql(configuration.GetConnectionString("Footballers"), ServerVersion.AutoDetect(configuration.GetConnectionString("Footballers"))));
+        }
+    }
+}
---
+++ Backend.Infrastructure/Seeders/FootballersSeeder.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.Seeders
+{
+    public class FootballersSeeder
+    {
+        private readonly FootballersDbContext _dbContext;
+        public FootballersSeeder(FootballersDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Seed()
+        {
+            if(await _dbContext.Database.CanConnectAsync())
+            {
+                if(!_dbContext.Footballers.Any())
+                {
+
+                }
+            }
+        }
+
+
+    }
+}
