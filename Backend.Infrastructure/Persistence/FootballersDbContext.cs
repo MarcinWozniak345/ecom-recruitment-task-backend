fdcfc7cfb8532814f39dbf00669432420698b004
MarcinWozniak345 <Marinox1234@wp.pl>
2025-10-10 15:18:29 +0200

    Create FootballersDbContext.cs

---
+++ Backend.Infrastructure/Persistence/FootballersDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.Persistence
+{
+    public partial class FootballersDbContext : DbContext
+    {
+        public DbSet<Domain.Entities.Footballer> Footballers { get; set; }
+
+        public FootballersDbContext(DbContextOptions<FootballersDbContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Entities.Footballer>()
+                .Property(e => e.Id).ValueGeneratedOnAdd();
+        }
+
+    }
+}
