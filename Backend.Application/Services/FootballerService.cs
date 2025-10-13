786a09117e6c7d975a9d35e89cc9c0614174f718
MarcinWozniak345 <Marinox1234@wp.pl>
2025-10-13 12:24:33 +0200

    Add FootballerService.cs and IFootballerService.cs

---
+++ Backend.Application/Services/FootballerService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Backend.Application.Models;
+using Backend.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Application.Services
+{
+    public class FootballerService : IFootballerService
+    {
+        private readonly IFootballerRepository _footballerRepository;
+        private readonly IMapper _mapper;
+        public FootballerService(IFootballerRepository footballerRepository, IMapper mapper)
+        { 
+            _footballerRepository = footballerRepository;
+            _mapper = mapper;
+        }
+
+        public List<FootballerDTO> GetAllFootballers()
+        {
+            var data = _footballerRepository.GetAllFootballers();
+            var result = _mapper.Map<List<FootballerDTO>>(data);
+            return result;
+        }
+
+        public FootballerDTO GetFootballer(Guid guid)
+        {
+            var data = _footballerRepository.GetFootballer(guid);
+            var result = _mapper.Map<FootballerDTO>(data);
+            return result;
+        }
+    }
+}
---
+++ Backend.Application/Services/IFootballerService.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Application.Services
+{
+    public interface IFootballerService
+    {
+        List<FootballerDTO> GetAllFootballers();
+        FootballerDTO GetFootballer(Guid guid);
+    }
+}
