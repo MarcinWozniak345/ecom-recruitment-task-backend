05017f9d4844cd35130de9d6aac67ed357c72326
MarcinWozniak345 <Marinox1234@wp.pl>
2025-10-13 12:22:17 +0200

    implementation of repository

---
+++ Backend.Infrastructure/Repositories/FootballerRepository.cs
@@ -0,0 +1,23 @@
+﻿using Backend.Domain.Entities;
+using Backend.Domain.Interfaces;
+using Backend.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.Repositories
+{
+    public class FootballerRepository : IFootballerRepository
+    {
+        private readonly FootballersDbContext _context;
+        public FootballerRepository(FootballersDbContext context) 
+        {
+            _context = context;
+        }
+        public List<Footballer> GetAllFootballers() => _context.Footballers.Where(x => x.IsDeleted == false).ToList();
+
+        public Footballer GetFootballer(Guid guid) => _context.Footballers.First(x => x.Guid == guid);
+    }
+}
