7c0e99b01b75f39cf66cf1bb522c72b22a1d1e04
MarcinWozniak345 <Marinox1234@wp.pl>
2025-10-13 12:27:32 +0200

    Add ServiceCollectionExtension to Application

---
+++ Backend.Application/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Application.Mappings;
+using Backend.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Application.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(FootbalersMappingProfile));
+            services.AddScoped<IFootballerService, FootballerService>();
+        }
+    }
+}
