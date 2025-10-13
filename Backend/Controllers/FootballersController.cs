6c59cbbd33cbef8bda750a6e347484ca905d1448
MarcinWozniak345 <Marinox1234@wp.pl>
2025-10-13 12:28:58 +0200

    Controller with my endpoints

---
+++ Backend/Controllers/FootballersController.cs
@@ -0,0 +1,32 @@
+﻿using Backend.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/Footballers")]
+    [ApiController]
+    public class FootballersController : ControllerBase
+    {
+        private readonly IFootballerService _footballerService;
+        public FootballersController(IFootballerService footballerService)
+        {
+            _footballerService = footballerService;
+        }
+
+
+        [HttpGet("GetAllFootballers")]
+        public ActionResult GetAllFootballers()
+        {
+            return Ok(_footballerService.GetAllFootballers());
+        }
+
+        [HttpGet("GetFootballer/{guid}")]
+        public ActionResult GetFootballers(Guid guid)
+        {
+
+            return Ok(_footballerService.GetFootballer(guid));
+        }
+
+    }
+}
