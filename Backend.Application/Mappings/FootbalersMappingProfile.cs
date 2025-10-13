1fa10b39efaf96b585548be508cbe44bfd924271
MarcinWozniak345 <Marinox1234@wp.pl>
2025-10-13 12:25:05 +0200

    Create Map wit automapper

---
+++ Backend.Application/Mappings/FootbalersMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Backend.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Application.Mappings
+{
+    public class FootbalersMappingProfile : Profile
+    {
+        public FootbalersMappingProfile() 
+        {
+            CreateMap<Backend.Domain.Entities.Footballer, FootballerDTO>();
+        }
+    }
+}
