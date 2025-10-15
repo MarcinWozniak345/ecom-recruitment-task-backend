1f6134849dfbacbca315d3481dd11afcdeb24fa4
MarcinWozniak345 <Marinox1234@wp.pl>
2025-10-15 20:33:02 +0200

    Update FootballersController.cs

--- Backend/Controllers/FootballersController.cs
+++ Backend/Controllers/FootballersController.cs
@@ -1,6 +1,8 @@
-﻿using Backend.Application.Services;
+﻿using Backend.Application.Models;
+using Backend.Application.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Backend.Controllers
 {
@@ -27,6 +29,41 @@ namespace Backend.Controllers
 
             return Ok(_footballerService.GetFootballer(guid));
         }
+
+        [HttpDelete("DeleteFootballer/{guid}")]
+        public async Task<ActionResult> DeleteFootballer(Guid guid)
+        {
+            await _footballerService.DeleteFootballer(guid);
+            return NoContent();
+        }
+
+        [HttpPost("CreateNewFootballer")]
+        public async Task<ActionResult> CreateNewFootballer([FromBody] CreateFootbalerDTO newFootballer)
+        {
+            var data = _footballerService.GetAllFootballers();
+            var exist = data.Any(x => x.Number == newFootballer.Number);
+            if (exist)
+            {
+                return BadRequest(new { message = "istnieje już piłarz o takim numerze na koszulce" });
+            }
+            await _footballerService.CreateFootballer(newFootballer);
+            return Ok(new { message = "Piłkarz został pomyślnie dodany." });
+
+        }
+
+        [HttpPut("UpdateFootballer")]
+        public async Task<ActionResult> UpdateFootballer([FromBody] UpdateFootbalerDTO updatedFootballer)
+        {
+            var data = _footballerService.GetAllFootballers();
+            var exist = data.Any(x => x.Number == updatedFootballer.Number && x.Guid != updatedFootballer.Guid);
+            if (exist)
+            {
+                return BadRequest(new { message = "istnieje już piłarz o takim numerze na koszulce" });
+            }
+            await _footballerService.UpdateFootballer(updatedFootballer);
+            return Ok(new { message = "Piłkarz został pomyślnie zmodyfikowany." }); ;
+        }
+
 
     }
 }
