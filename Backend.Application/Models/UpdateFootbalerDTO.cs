91cc982efdca966c333a5a445390ed20dda5bfbc
MarcinWozniak345 <Marinox1234@wp.pl>
2025-10-15 20:34:07 +0200

    Created CreateFootbalerDTO.cs and UpdateFootbalerDTO.cs

---
+++ Backend.Application/Models/UpdateFootbalerDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Application.Models
+{
+    public class UpdateFootbalerDTO
+    {
+        public string Name { get; set; } = default!;
+        public string Surname { get; set; } = default!;
+        public int Number { get; set; }
+        public string Position { get; set; } = default!;
+        public string Nationality { get; set; } = default!;
+        public int NumberOfMatches { get; set; }
+        public int Goals { get; set; }
+        public int Assists { get; set; }
+        public int Age { get; set; }
+        public Guid Guid { get; set; }
+    }
+}
